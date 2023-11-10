cf5c73ba89adbea9bc06049d583579ed53ce8573
Kirill Vasko <kirill.vasko.2003@gmail.com>
2023-11-10 22:27:55 +0300

    fix: GitHub action

--- src/decorators/withRange/index.tsx
+++ src/decorators/withRange/index.tsx
@@ -1,13 +1,8 @@
-import React, { ComponentType } from "react";
+import { ComponentType } from "react";
 
 import { CalendarProps } from "components/Calendar/types";
 
-import { Range } from "./types";
-
-interface WithRangeProps extends Pick<CalendarProps, "isWithRange"> {
-  range?: Range;
-  setRange?: React.Dispatch<React.SetStateAction<Range | undefined>>;
-}
+interface WithRangeProps extends Pick<CalendarProps, "isWithRange"> {}
 
 const withRange = <T extends WithRangeProps>(
   WrappedComponent: ComponentType<T>,
