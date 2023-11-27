a06eb9fe199342b09ca4bb91929716d09a50ec10
Kirill Vasko <kirill.vasko.2003@gmail.com>
2023-11-27 17:54:44 +0300

    fix: index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-export { default as CalendarService } from "./decorators";
-export { default as withHolidays } from "./decorators/withHolidays";
-export { default as withMinAndMaxDate } from "./decorators/withMinAndMaxDate";
-export { default as withMondayFirst } from "./decorators/withMondayFirst";
-export { default as withRange } from "./decorators/withRange";
-export { default as withTodos } from "./decorators/withTodos";
-export { default as withWeekends } from "./decorators/withWeekends";
+export { default as CalendarService } from "decorators";
+export { default as withHolidays } from "decorators/withHolidays";
+export { default as withMinAndMaxDate } from "decorators/withMinAndMaxDate";
+export { default as withMondayFirst } from "decorators/withMondayFirst";
+export { default as withRange } from "decorators/withRange";
+export { default as withTodos } from "decorators/withTodos";
+export { default as withWeekends } from "decorators/withWeekends";
 
-export { useDate } from "./providers/DateProvider";
+export { useDate } from "providers/DateProvider";
 
-export { default as DatePicker } from "./components/DatePicker";
+export { default as DatePicker } from "components/DatePicker";
